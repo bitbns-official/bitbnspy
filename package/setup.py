55aa4c770e06311ef14cdc2858bfa217617243e4
Tanmesh Mishra <tanmeshmishra@Tanmeshs-MacBook-Pro.local>
2020-06-01 14:13:38 +0530

    Updated author, author_email

--- package/setup.py
+++ package/setup.py
@@ -16,13 +16,20 @@ setup(
     install_requires=[  # I get to this in a second
         'validators',
         'beautifulsoup4',
+        'requests',
+        'socketio',
+        'time',
+        'base64',
+        'hashlib',
+        'hmac',
+        'json',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Developers',  # Define that your audience are developers
+        'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',  # Again, pick a license
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.7.3',
     ],
 )
