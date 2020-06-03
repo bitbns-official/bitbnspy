63aef108706af3b0e9d5ef7ef205d968c230cc33
Tanmesh Mishra <tanmeshmishra@Tanmeshs-MacBook-Pro.local>
2020-06-03 16:43:05 +0530

    Updated setup.py

--- package/setup.py
+++ package/setup.py
@@ -1,24 +1,28 @@
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup
 
 from pkg_resources import parse_requirements
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='bitbns',
     packages=['bitbns'],
-    version='0.7',
+    version='0.8',
     license='MIT',
     description='This project is designed to assist you make your own projects that interact with the Bitbns API. '
                 'This project seeks to have complete API coverage excluding WebSockets which would be released in '
                 'the future version.',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Prashant Singh',
     author_email='prashant@buyhatke.com',
     url='https://github.com/bitbns-official/python-bitbns-api',
     # Provide either the link to your github or to your website
-    download_url='https://github.com/bitbns-official/python-bitbns-api/archive/0.7.tar.gz',
+    download_url='https://github.com/bitbns-official/python-bitbns-api/archive/0.8.tar.gz',
     keywords=['npm', 'bitbns', 'crypto', 'btc', 'eth', 'neo', 'orderbook', 'crypto trading', 'bitbns api'],
     install_reqs=parse_requirements('requirements.txt'),
     classifiers=[
