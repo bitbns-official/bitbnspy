84758bf18948695ed4c5e2e8f3b456f5c40266a3
Tanmesh Mishra <tanmeshmishra@Tanmeshs-MacBook-Pro.local>
2020-06-03 15:56:58 +0530

    Updated setup.py

--- package/setup.py
+++ package/setup.py
@@ -2,8 +2,10 @@ from distutils.core import setup
 
 from pkg_resources import parse_requirements
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='bitbns',
