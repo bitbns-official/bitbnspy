fb91a829a0a7a7a4e99d40d771d54ed9f99fe6c6
Tanmesh Mishra <tanmeshmishra@Tanmeshs-MacBook-Pro.local>
2020-06-15 13:41:48 +0530

    Updated requirements.txt

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
 # from distutils.core import setup
 from setuptools import setup
 
-from pkg_resources import parse_requirements
+import pathlib
+
+import pkg_resources
+
 
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
 setup(
     name='bitbnspy',
     packages=['bitbnspy'],
-    version='0.1',
+    version='0.1.19',
     license='MIT',
     description='This project is designed to assist you make your own projects that interact with the Bitbns API. '
                 'This project seeks to have complete API coverage excluding WebSockets which would be released in '
@@ -24,7 +34,7 @@ setup(
     # Provide either the link to your github or to your website
     download_url='',
     keywords=['npm', 'bitbns', 'crypto', 'btc', 'eth', 'neo', 'orderbook', 'crypto trading', 'bitbns api'],
-    install_reqs=parse_requirements('requirements.txt'),
+    install_reqs=install_requires,
     classifiers=[
         'Development Status :: 4 - Beta',
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
