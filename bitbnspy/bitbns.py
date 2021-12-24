b14d7aea5d418cb8759e1ca08c37f31e6265671f
its-mohnish <84853776+its-mohnish@users.noreply.github.com>
2021-12-24 14:28:30 +0400

    Added endpoint fetchMarkets

--- bitbnspy/bitbns.py
+++ bitbnspy/bitbns.py
@@ -464,7 +464,15 @@ class bitbns():
             return {'data': data[:limit], 'error': None, 'status': 1}
         except Exception as e:
             return self.genErrorMessage(None, 0, f'some error in get req :{e}')
-
+   
+    def fetchMarkets(self):
+        try:
+            req = self.connectionsAdaptor.get(self.baseUrl3 + 'order/fetchMarkets/')
+            data = req.json()
+            return {'data': data, 'error': None, 'status': 200}
+        except Exception as e:
+            return self.genErrorMessage(None, 0, f'some error in get req :{e}')
+        
     #still in dev (endpoint maybe updated later)
     def fetchOHLCV(self, coin_name: str, market_name: str, page: int = 1):
         try:
