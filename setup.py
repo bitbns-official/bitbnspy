cd987f22e3b948ff838208437eb2434b4bbce3db
its-mohnish <84853776+its-mohnish@users.noreply.github.com>
2022-04-18 12:53:35 +0530

    Added more keywords

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     author_email='prashant@buyhatke.com',
     url='https://github.com/bitbns-official/bitbnspy',
     # download_url='https://github.com/bitbns-official/bitbnspy/archive/0.1.tar.gz',
-    keywords=['python', 'bitbns', 'DEFI', 'crypto', 'btc', 'eth', 'neo', 'orderbook', 'crypto trading', 'bitbns api', 'margin trading', 'partner apis', 'payment gatway'],
+    keywords=['python', 'bitbns', 'DEFI', 'crypto', 'btc', 'eth', 'neo', 'orderbook', 'crypto trading', 'bitbns api', 'margin trading', 'partner apis', 'payment gatway', 'fips'],
     install_requires=requirements,
     classifiers=[
         'Development Status :: 4 - Beta',
